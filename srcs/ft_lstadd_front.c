a518410c78112e4652b61ae82e294f96230d2f4c
shintaro kohtake <shintarokohtake@shintaros-MacBook-Air.local>
2025-07-17 08:29:49 +0900

    add lstadd_front

---
+++ srcs/ft_lstadd_front.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shintarokohtake <shintarokohtake@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 22:21:59 by shintarokoh       #+#    #+#             */
+/*   Updated: 2025/07/16 23:39:45 by shintarokoh      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if(new == NULL || lst == NULL)
+		return;
+	new->next = *lst;
+	*lst = new;
+}
