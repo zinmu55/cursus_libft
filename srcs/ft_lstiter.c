9a7f28b38d8653b8a8c778005e3ed9042f154120
shintaro kohtake <shintarokohtake@shintaros-MacBook-Air.local>
2025-07-19 11:50:04 +0900

    follow norm

--- srcs/ft_lstiter.c
+++ srcs/ft_lstiter.c
@@ -12,13 +12,13 @@
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if(!lst || !f)
-		return;
+	if (!lst || !f)
+		return ;
 	else
 	{
-		while(lst)
+		while (lst)
 		{
 			f(lst->content);
 			lst = lst->next;
--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -21,16 +21,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst || !f || !del)
 		return (NULL);
 	new_lst_head = NULL;
-	while(lst)
+	while (lst)
 	{
 		tmp = f(lst->content);
 		new_node = ft_lstnew(tmp);
-		if(!new_node)
+		if (!new_node)
 		{
-			ft_lstclear(&new_lst_head,del);
-			return(NULL);
+			ft_lstclear(&new_lst_head, del);
+			return (NULL);
 		}
-		
 		ft_lstadd_back(&new_lst_head, new_node);
 		lst = lst->next;
 	}
