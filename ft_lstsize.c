9c5eab2903a772402086a8c5515d69404a51b5e8
Shintaro Kohtake <skohtake@c5r3s7.42tokyo.jp>
2025-07-26 13:25:46 +0900

    modify lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 20:23:07 by shintarokoh       #+#    #+#             */
-/*   Updated: 2025/07/26 12:17:23 by skohtake         ###   ########.fr       */
+/*   Updated: 2025/07/26 13:20:58 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,10 @@ int	ft_lstsize(t_list *lst)
 	int	i;
 
 	i = 0;
-	if (lst != NULL)
+	while (lst)
 	{
+		lst = lst->next;
 		i++;
-		while (lst->next)
-		{
-			lst = lst->next;
-			i++;
-		}
 	}
 	return (i);
 }
